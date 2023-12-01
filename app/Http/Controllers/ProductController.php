ceb8ebf3dfa2c45c640c0a41ca3b07221fd62831
Muid-Hasan <muid1994@gmail.com>
2023-12-02 00:03:36 +0600

    third Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\ProductDetail;
+use App\Models\ProductReview;
+use App\Models\ProductSlider;
+use App\Helper\ResponseHelper;
+use Illuminate\Http\JsonResponse;
 
 class ProductController extends Controller
 {
-    //
+    public function ListProductByCategory(Request $request):JsonResponse{
+        $data=Product::where("category_id","=", $request->id)->with('brand','category')->get();
+        return ResponseHelper::Out('success',$data,200);
+    }
+    public function ListProductByRemark(Request $request):JsonResponse{
+        $data=Product::where('remark','=', $request->remark)->with('brand','category')->get();
+        return ResponseHelper::Out('success',$data,200);
+    }
+    public function ListProductByBrand(Request $request):JsonResponse{
+        $data=Product::where('brand_id','=',$request->id)->with('brand','category')->get();
+        return ResponseHelper::Out('success',$data,200);
+    }
+    public function ListProductSlider():JsonResponse{
+        $data=ProductSlider::all();
+        return ResponseHelper::Out('success',$data,200);
+    }
+    public function ProductDetailsById(Request $request):JsonResponse{
+        $data=ProductDetail::where('product_id','=', $request->id)->with('product','product.brand','product.category')->get();
+        return ResponseHelper::Out('success',$data,200);
+    }
+    public function ListReviewByProduct(Request $request):JsonResponse{
+        $data=ProductReview::where('product_id','=', $request->id)->with(['product'=>function($query){
+            $query->select('id','cus_name');
+        }])->get();
+        return ResponseHelper::Out('success',$data,200);
+    }
+
 }
